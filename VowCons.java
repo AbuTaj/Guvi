dc86e3edc829c7bf7da3926ae62b28b8e7ece73d
Abu Taj <abu786sagar@gmail.com>
2016-07-23 02:57:41 +0530

    Update VowCons.java

--- VowCons.java
+++ VowCons.java
@@ -7,7 +7,9 @@ public class VowCons {
 	public static void main(String[] args) {
 		System.out.println("Enter the Character to Check");
 		s = new Scanner(System.in);
-		char c = s.next().charAt(0);
+		String s1 = s.next();
+		s1 = s1.toLowerCase();
+		char c = s1.charAt(0);
 		if ((c == 'a') || (c == 'e') || (c == 'i') || (c == 'o') || (c == 'u')) {
 			System.out.println("The given character is Vowel");
 		} else {
