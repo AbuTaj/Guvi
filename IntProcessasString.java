192e8a79fb4c6cd4f2945589a8f088838a21aaf8
Abu Taj <abu786sagar@gmail.com>
2016-07-30 23:56:02 +0530

    Create IntProcessasString.java

---
+++ IntProcessasString.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class StringMul {
+
+	private static Scanner s;
+
+	public static void main(String[] args) {
+		s = new Scanner(System.in);
+		System.out.println("Enter the First Number as String");
+		String s1 = s.next();
+		int a = Integer.parseInt(s1);
+		System.out.println("Enter the Second Number as String");
+		String s2 = s.next();
+		int b = Integer.parseInt(s2);
+		int rs = a * b;
+		System.out.println("The Multiplication of the Number is : " + Integer.toString(rs));
+
+	}
+
+}
