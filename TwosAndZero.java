e89d050342a3c7f386de3ff4806e242653b9ceb9
Abu Taj <abu786sagar@gmail.com>
2016-08-31 10:33:00 +0530

    Update TwosAndZero.java

--- TwosAndZero.java
+++ TwosAndZero.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class TwosAndZeros {
 
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		String s1 = s.next();
-		String sr[] = s1.split(",");
-		int count = 0;
+		Scanner ss = new Scanner(System.in);
+		String s2 = s.next();
+		String sr1[] = s1.split(",");
+		int count1 = 0;
 		for (int i = 0; i < sr.length - 1; i++) {
 			if (sr[i].equals("0") && sr[i + 1].equals("2")
 					|| sr[i + 1].equals("2")) {
