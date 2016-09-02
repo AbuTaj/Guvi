7e3e15e6734f42b9e20e8ce91d4f34e2d1b2c36d
Abu Taj <abu786sagar@gmail.com>
2016-09-02 12:14:06 +0530

    Create PosNegZero.py

---
+++ PosNegZero.py
@@ -0,0 +1,13 @@
+'''
+Created on 02-Sep-2016
+
+@author: AbuTaj
+'''
+num = int(input('Enter the number \n'))
+if(num != 0):
+    if(num < 0):
+        print('Negative')
+    else:
+        print('Positive')
+else:
+    print('Zero')
