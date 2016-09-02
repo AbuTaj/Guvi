693c74faeb0dcee88c5861849b7cd885c760dc5f
Abu Taj <abu786sagar@gmail.com>
2016-09-02 16:30:18 +0530

    Create VowOrCons.py

---
+++ VowOrCons.py
@@ -0,0 +1,7 @@
+# Created on 02-Sep-2016
+# @author: AbuTaj
+s = input('Enter Char')[0].lower()
+if(s == 'a'or s == 'e'or s == 'i'or s == 'o'or s == 'u'):
+    print('Vowel')
+else:
+    print('Constant')
