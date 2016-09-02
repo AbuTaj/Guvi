03849adbbc36320dd983fdcc89aac38082011207
Abu Taj <abu786sagar@gmail.com>
2016-09-02 16:37:59 +0530

    Create LargestOf3.py

---
+++ LargestOf3.py
@@ -0,0 +1,11 @@
+# Created on 02-Sep-2016
+# @author: abu78
+num1 = input('Enter Num 1')
+num2 = input('Enter Num 2')
+num3 = input('Enter Num 3')
+if num1 > num2 and num1 > num3:
+    print('Num 1 is greater : ', num1)
+elif num2 > num1 and num2 > num3:
+    print('Num 2 is greater : ', num2)
+else:
+    print('Num 3 is greater : ', num3)
