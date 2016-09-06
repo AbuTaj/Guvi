3e4d74c6002428228da05b6f47d62c1db1eeb08a
Abu Taj <abu786sagar@gmail.com>
2016-09-06 18:49:01 +0530

    Update ProperTags.java

--- ProperTags.java
+++ ProperTags.java
@@ -7,19 +7,22 @@ public class ProperTags {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		String s1 = s.nextLine();
-		int op = 0;
-		int cl = 0;
+		float op = 0f;
+		float cl = 0f;
 		String sr[] = s1.split(" ");
 		for (int i = 0; i < sr.length; i++) {
-			if (sr[i].charAt(0) == '<'
-					&& sr[i].charAt(sr[i].length() - 1) == '>') {
-				op += 1;
-			}
-			if (sr[i].charAt(0) == '<' && sr[i].charAt(1) == '/'
-					&& sr[i].charAt(sr[i].length() - 1) == '>') {
-				cl += 1;
+			if (sr[i].length() > 2) {
+				if (sr[i].charAt(0) == '<'
+						&& sr[i].charAt(sr[i].length() - 1) == '>') {
+					op += 1;
+				}
+				if (sr[i].charAt(0) == '<' && sr[i].charAt(1) == '/'
+						&& sr[i].charAt(sr[i].length() - 1) == '>') {
+					cl += 1;
+				}
 			}
 		}
+		System.out.println(op + " " + cl);
 		op /= 2;
 		if (op == cl) {
 			System.out.println("Proper");
@@ -28,5 +31,4 @@ public class ProperTags {
 		}
 		s.close();
 	}
-
 }
